33ff9424987b8936cf16eabb7c6cc452a3264958
Manahill Naeem <165715242+Manahill-Naeem@users.noreply.github.com>
2025-07-31 17:51:32 +0500

    Fix: Corrected case sensitivity for models folder/Sale.js

--- routes/emailRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/emailRoutes.js
-const express = require('express');
-const router = express.Router();
-const { sendContactEmail } = require('../controllers/emailController');
-
-// Define the route for sending contact emails
-router.post('/contact', sendContactEmail);
-
-module.exports = router;
